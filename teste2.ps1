105b0f6aaf5dcbc8db9a97c9418a4f5b6ee1a9f1
gean3721 <110415440+gean3721@users.noreply.github.com>
2023-08-16 16:00:44 -0300

    teste2.ps1

--- teste2.ps1
+++ teste2.ps1
@@ -1,23 +1,11 @@
-﻿$url = "https://github.com/gean3721/executaveis/raw/main/scriptcnc.exe"
-$destination = [System.Environment]::GetFolderPath("Desktop") + "\scriptcnc.exe"
+$url = "https://www.example.com/arquivo.exe"
+$destination = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "arquivo.exe")
 
 Invoke-WebRequest -Uri $url -OutFile $destination
 
 # Verifica se o download foi bem-sucedido
 if (Test-Path $destination) {
     Start-Process -FilePath $destination
-
-    # Agendar a exclusão após 1 minuto
-    $timer = New-Object System.Timers.Timer
-    $timer.Interval = 60000 # 1 minuto em milissegundos
-    $timer.AutoReset = $false
-    $timer.Enabled = $true
-
-    Register-ObjectEvent -InputObject $timer -EventName Elapsed -Action {
-        # Deleta o arquivo após 1 minuto
-        Remove-Item -Path $destination -Force
-        $timer.Dispose()
-    }
 } else {
     Write-Host "Erro ao baixar o arquivo."
 }
